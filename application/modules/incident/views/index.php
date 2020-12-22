801afb05ed9f0e4e7fa8f1e0cc56b71d550b2970
firmanpurnama <firman.purnama.h@gmail.com>
2020-12-22 16:55:48 +0700

    update mockup ui

--- application/modules/incident/views/index.php
+++ application/modules/incident/views/index.php
@@ -1,30 +1,175 @@
 <div class="bg-grey">
-    <div class="uk-flex uk-flex-right uk-position-top p-10 m-5">
+    <!-- <div class="uk-flex uk-flex-right uk-position-top p-10 m-5">
         <div class="uk-background-secondary text-white p-5">
             <span class="uk-margin-small-right" uk-icon="icon: plus"></span>
             <span class="uk-margin-small-right" uk-icon="icon: list"></span>
             <span class="uk-margin-small-right" uk-icon="icon: search"></span>
         </div>
-    </div>
+    </div> -->
     <div class="uk-padding-small">
-        <div>
-            <h3>Incident Type</h3>
-            <span>
-            What is Incident type as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
-            </span>
-        </div>
+        <div uk-grid>
+            <div class="uk-width-2-3@l uk-width-1-2@s">
+                <div class="uk-flex">
+                    <div class="uk-width-1-2">
+                        <h3>Incident Type</h3>
+                    </div>
+                    
+                    <div class="uk-width-1-2">
+                        <div class="uk-flex uk-flex-right p-10">
+                            <div class="uk-background-secondary text-white p-5">
+                                <span class="uk-margin-small-right" uk-icon="icon: plus"></span>
+                                <span class="uk-margin-small-right" uk-icon="icon: list"></span>
+                                <span class="uk-margin-small-right" uk-icon="icon: search"></span>
+                            </div>
+                        </div>
+                    </div>            
+                </div>
 
-        <div class="uk-flex">
-            <div>
-                <span class="uk-margin-small-right" uk-icon="icon: search"></span>
+                <span>What is Incident type as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua ini et dolore magna aliqua ini</span>
+
+                <div class="uk-flex" uk-toggle="target: #my-id">
+                    <div>
+                        <span class="uk-margin-small-right" uk-icon="icon: folder; ratio: 2.5"></span>
+                    </div>
+                    <div>
+                        <div class="uk-flex">
+                            <div class="uk-width-1-2">
+                                <h2>Thief</h2>
+                            </div>
+                            <div class="uk-width-1-2 uk-text-right">
+                                <span>2 min ago</span>
+                            </div>
+                        </div>
+                        <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                        <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                    </div>
+                </div>
+                
+                <div class="uk-flex" uk-toggle="target: #my-id">
+                    <div>
+                        <span class="uk-margin-small-right" uk-icon="icon: folder; ratio: 2.5"></span>
+                    </div>
+                    <div>
+                        <div class="uk-flex">
+                            <div class="uk-width-1-2">
+                                <h2>Tsunami</h2>
+                            </div>
+                            <div class="uk-width-1-2 uk-text-right">
+                                <span>2 year ago</span>
+                            </div>
+                        </div>
+                        <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                        <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                    </div>
+                </div>
             </div>
-            <div>
-                <h2>Thief</h2>
-                <span>
-                Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua
-                </span>
+            
+            <div id="my-id" class="uk-width-1-3">
+                <div class="">
+                    <button class="uk-offcanvas-close" type="button" uk-toggle="target: #my-id" uk-close></button>
+                    <div class="uk-flex">
+                        <div>
+                            <span class="uk-margin-small-right" uk-icon="icon: folder; ratio: 1.5"></span>
+                        </div>
+                        <div>
+                            <h3>Thief</h3>
+                        </div>
+                    </div>
+                    <div>
+                        <span>Description</span>
+                        <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                        <h3>Incident Type Form</h3>
+                        <form action="#" class="uk-form-stacked">
+                            <div class="uk-margin">
+                                <label class="uk-form-label" for="form-stacked-text">Type</label>
+                                <div class="uk-form-controls">
+                                    <input class="uk-input" id="form-stacked-text" type="text" placeholder="Some text...">
+                                </div>
+                            </div>
+                            <div class="uk-margin">
+                                <label class="uk-form-label" for="form-stacked-text">Description</label>
+                                <div class="uk-form-controls">
+                                    <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+                                </div>
+                            </div>
+                            <button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Submit</button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
+        <!-- end grid -->
+
+        <!-- <div class="uk-flex" uk-toggle="target: #offcanvas-incident; container: #nah;">
+            <div>
+                <span class="uk-margin-small-right" uk-icon="icon: folder; ratio: 1.5"></span>
+            </div>
+            <div>
+                <div class="uk-flex">
+                    <div class="uk-width-1-2">
+                        <h2>Thief</h2>
+                    </div>
+                    <div class="uk-width-1-2 uk-text-right">
+                        <span>2 min ago</span>
+                    </div>
+                </div>
+                <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+            </div>
+        </div> -->
+        
+        <!-- <div class="uk-flex" uk-toggle="target: #offcanvas-incident">
+            <div>
+                <span class="uk-margin-small-right" uk-icon="icon: folder; ratio: 1.5"></span>
+            </div>
+            <div>
+                <div class="uk-flex">
+                    <div class="uk-width-1-2">
+                        <h2>Tsunami</h2>
+                    </div>
+                    <div class="uk-width-1-2 uk-text-right">
+                        <span>2 year ago</span>
+                    </div>
+                </div>
+                <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+                <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+            </div>
+        </div>         -->
     </div>
 </div>
+
+<!-- canvas -->
+<!-- <div id="offcanvas-incident" uk-offcanvas="flip: true">
+    <div class="uk-offcanvas-bar">
+        <button class="uk-offcanvas-close" type="button" uk-close></button>
+        <div class="uk-flex">
+            <div>
+                <span class="uk-margin-small-right" uk-icon="icon: folder; ratio: 1.5"></span>
+            </div>
+            <div>
+                <h3>Thief</h3>
+            </div>
+        </div>
+        <div>
+            <span>Description</span>
+            <span>Description of theft as Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</span>
+            <h3>Incident Type Form</h3>
+            <form action="#" class="uk-form-stacked">
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Type</label>
+                    <div class="uk-form-controls">
+                        <input class="uk-input" id="form-stacked-text" type="text" placeholder="Some text...">
+                    </div>
+                </div>
+                <div class="uk-margin">
+                    <label class="uk-form-label" for="form-stacked-text">Description</label>
+                    <div class="uk-form-controls">
+                        <textarea class="uk-textarea" rows="5" placeholder="Textarea"></textarea>
+                    </div>
+                </div>
+                <button class="uk-button uk-button-primary uk-width-1-1 uk-margin-small-bottom">Submit</button>
+            </form>
+        </div>
+    </div>
+</div> -->
+<!-- end canvas -->
