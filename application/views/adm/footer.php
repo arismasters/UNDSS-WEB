0f433dc8de218ce944f3c03a781492ea1c9783a5
Megan <31150796+Megareceiver@users.noreply.github.com>
2021-03-15 14:17:19 +0700

    Empty branch

--- application/views/adm/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="uk-background-secondary">
-    <div class="uk-margin-small-left">
-        <span class="uk-text-italic uk-text-meta">
-            &copy; 2020 undss.
-        </span>
-    </div>
-</footer>
